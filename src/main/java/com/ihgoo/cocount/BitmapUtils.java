e9b2f499c8e86f1de2809e27d32df87d931cf862
ihgoo <ihgoo2008@gmail.com>
2015-03-02 12:58:15 +0800

    add method

--- src/main/java/com/ihgoo/cocount/BitmapUtils.java
+++ src/main/java/com/ihgoo/cocount/BitmapUtils.java
@@ -1,23 +1,30 @@
 package com.ihgoo.cocount;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-
-import com.ihgoo.cocount.cache.ImageSDCacher;
-import com.ihgoo.cocount.util.LogUtils;
-import com.ihgoo.cocount.util.ScreenUtil.Screen;
-
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+
+import com.ihgoo.cocount.cache.ImageSDCacher;
+import com.ihgoo.cocount.util.LogUtils;
+import com.ihgoo.cocount.util.ScreenUtil.Screen;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 /**
  * 对Bitmap二次处理，避免OOM异常出现，
@@ -588,6 +595,67 @@ public class BitmapUtils {
 		}
 
 	}
+
+
+
+
+    /**
+     * 缩放图片
+     *
+     * @param bitmap
+     * @param f
+     * @return
+     */
+    public static Bitmap zoom(Bitmap bitmap, float zf) {
+        Matrix matrix = new Matrix();
+        matrix.postScale(zf, zf);
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(),
+                bitmap.getHeight(), matrix, true);
+    }
+
+    /**
+     * 缩放图片
+     *
+     * @param bitmap
+     * @param f
+     * @return
+     */
+    public static Bitmap zoom(Bitmap bitmap, float wf, float hf) {
+        Matrix matrix = new Matrix();
+        matrix.postScale(wf, hf);
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(),
+                bitmap.getHeight(), matrix, true);
+    }
+
+    /**
+     * 图片圆角处理
+     *
+     * @param bitmap
+     * @param roundPX
+     * @return
+     */
+    public static Bitmap getRCB(Bitmap bitmap, float roundPX) {
+        // RCB means
+        // Rounded
+        // Corner Bitmap
+        Bitmap dstbmp = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(dstbmp);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawRoundRect(rectF, roundPX, roundPX, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return dstbmp;
+    }
+
+
 	/**
 	 * 根据图片的路径获取图片的大小
 	 * 
